1718d1334aa9f3a44f7334b148fb82fdf99d3745
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-11 11:36:40 -0700

    Added Repos component

---
+++ src/Components/Repos/Repos.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Repos.css'
+
+const Repos = () => {
+	return (
+		<div className='repos-container'>
+			<h2>I'm the Repos Container</h2>
+		</div>
+	)
+}
+
+
+export default Repos;

0c26e2ae0dac1b842990f6a0dc5ae35c5be3e63b
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-11 13:40:52 -0700

    Update RepoCard component

--- src/Components/RepoCard/RepoCard.js
+++ src/Components/RepoCard/RepoCard.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './RepoCard.css'
 
-const RepoCard = () => {
+const RepoCard = ({ repo }) => {
+	const {name, language, description, starCount, forkCount, dateCreated} = repo;
+	
 	return (
 		<div className='repo-card'>
-			<h2>I'm a repo card</h2> 
+			<h2>{name}</h2> 
+			<p>{language}</p>
+			<p>{description}</p>
+			<p>{starCount}</p>
+			<p>{forkCount}</p>
+			<p>{dateCreated}</p>
 		</div>
 	)
 }

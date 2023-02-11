04574949c6b267e9075b78983bd46f7a14f3d9d4
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-11 11:35:52 -0700

    Update App component

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Repos from '../Repos/Repos'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+			<header>Welcome to RepoHub 😎</header>
+			<Repos />
+    </div>
+  );
+}
+
+export default App;

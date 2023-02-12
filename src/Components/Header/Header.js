3c23c6f232cc2ee56c3f13a35ca8a9ffd660fff4
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-11 23:22:10 -0700

    Added Header styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
 	return (
 		<div className='header-container'>
-			<h1>Welcome to RepoHub 😎</h1>
+			<h1>Welcome to RepoHub</h1>
 			<h3>A site where you can find an organizations Github repositories!</h3>
 		</div>
 	)

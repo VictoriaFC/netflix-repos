09c2bf13ce0aa7355d39b67af2c3158d30a167da
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-11 20:51:59 -0700

    Updated Repos

--- src/Components/Repos/Repos.js
+++ src/Components/Repos/Repos.js
@@ -1,40 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import RepoCard from '../RepoCard/RepoCard'
-import { fetchRepos } from '../../apiCalls'
 import './Repos.css'
 
-const Repos = ({ repos, setRepos }) => {
+const Repos = ({ orgName, repos }) => {
 
-	const getRepos = async () => {
-		const reposData = await fetchRepos();
-		const refinedReposData = reposData.map((repoData, index) => {
-			return {
-				name: repoData.name,
-				language: repoData.language, 
-				description: repoData.description,
-				starCount: repoData.stargazers_count,
-				forkCount: repoData.forks_count,
-				dateCreated: new Date(Date.parse(repoData.created_at)),
-				commitsUrl: repoData.commits_url,
-				key: index
-			}
-		})
-		setRepos(sortReposByStarCount(refinedReposData));
-	}
-
-	useEffect(() => {
-		getRepos();
-	}, [])
-
-	const sortReposByStarCount = (repos) => {
-		const sortedRepos = repos.sort((a, b) => {
-			return b.starCount - a.starCount
-		})
-		
-		return sortedRepos;
-	}
-
-	const repoCards = repos.map((repo) => <RepoCard key={repo.key} repo={repo}/>)
+	const repoCards = repos.map((repo) => <RepoCard key={repo.key} orgName={orgName} repo={repo}/>)
 	
 	return (
 		<div className='repos-container'>

933727b377413dbd69ff691af017332159f177d7
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-11 18:13:44 -0700

    Updated App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Repos from '../Repos/Repos'
 import Commits from '../Commits/Commits'
+import Search from '../Search/Search'
 import './App.css';
 
 const App = () => {
+	const [repos, setRepos] = useState([]);
+	const [searchInput, setSearchInput] = useState('')
 
   return (
 		<div className="App">
-			<header>Welcome to RepoHub 😎</header>
+			<h1>Welcome to RepoHub 😎</h1>
+			<h3>A site where you can find an organizations Github repositories!</h3>
 			<Route exact path="/">
-				<Repos />
+				<Search searchInput={searchInput} setSearchInput={setSearchInput}/>
+				<Repos repos={repos} setRepos={setRepos} />
 			</Route>
 
 			<Route exact path="/Commits/:id"

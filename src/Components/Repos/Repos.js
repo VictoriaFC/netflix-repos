5b7991444160038ae05a61fd6d3d6ac2f41173c4
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-11 17:31:45 -0700

    Update Repo.js

--- src/Components/Repos/Repos.js
+++ src/Components/Repos/Repos.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import RepoCard from '../RepoCard/RepoCard'
-import fetchRepos from '../../apiCalls'
+import { fetchRepos } from '../../apiCalls'
 import './Repos.css'
 
 const Repos = () => {
@@ -15,22 +15,30 @@ const Repos = () => {
 				description: repoData.description,
 				starCount: repoData.stargazers_count,
 				forkCount: repoData.forks_count,
-				dateCreated: repoData.created_at,
+				dateCreated: new Date(Date.parse(repoData.created_at)),
+				commitsUrl: repoData.commits_url,
 				key: index
 			}
 		})
-		setRepos(refinedReposData)
+		setRepos(sortReposByStarCount(refinedReposData));
 	}
 
 	useEffect(() => {
 		getRepos();
 	}, [])
 
+	const sortReposByStarCount = (repos) => {
+		const sortedRepos = repos.sort((a, b) => {
+			return b.starCount - a.starCount
+		})
+		
+		return sortedRepos;
+	}
+
 	const repoCards = repos.map((repo) => <RepoCard key={repo.key} repo={repo}/>)
 	
 	return (
 		<div className='repos-container'>
-			<h2>I'm the Repos Container</h2>
 			{repoCards}
 		</div>
 	)

8e5cf6a06e7d6f5585d5c2db4bd4ad589661422e
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-11 17:30:49 -0700

    Update RepoCard.js

--- src/Components/RepoCard/RepoCard.js
+++ src/Components/RepoCard/RepoCard.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './RepoCard.css'
 
 const RepoCard = ({ repo }) => {
-	const {name, language, description, starCount, forkCount, dateCreated} = repo;
-	
+	const {name, language, description, starCount, forkCount, dateCreated, commitsUrl} = repo;
+
 	return (
-		<div className='repo-card'>
-			<h2>{name}</h2> 
-			<p>{language}</p>
-			<p>{description}</p>
-			<p>{starCount}</p>
-			<p>{forkCount}</p>
-			<p>{dateCreated}</p>
-		</div>
+		// <Link to={`/Commits/${repo.name}`}>
+			<div className='repo-card'>
+				<Link to={`/Commits/${repo.name}`}>
+					<h2>{name}</h2> 
+				</Link>
+				<p>{language}</p>
+				<p>{description}</p>
+				<p>Star Count: {starCount}</p>
+				<p>Fork Count: {forkCount}</p>
+				<p>Date Created: {(dateCreated).toDateString()}</p>
+			</div>
+		// </Link>
 	)
 }
 

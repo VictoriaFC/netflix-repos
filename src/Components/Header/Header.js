6ddc9234fcb02fa711a6fc0764037d60e0d34ce6
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-11 20:53:49 -0700

    Added Header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+	return (
+		<div className='header-container'>
+			<h1>Welcome to RepoHub 😎</h1>
+			<h3>A site where you can find an organizations Github repositories!</h3>
+		</div>
+	)
+}
+export default Header;

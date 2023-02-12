fec4691739524deb897fdb8c4855b00a0d655f4b
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-11 17:29:35 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import App from './Components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-		<BrowserRouter>
-    	<App />
-		</BrowserRouter>
-  </React.StrictMode>
+	<BrowserRouter>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</BrowserRouter>
 );
 

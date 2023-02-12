699f9a97e43087888989547394d2d43484aa0d40
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-11 18:14:38 -0700

    Updated RepoCard

--- src/Components/RepoCard/RepoCard.js
+++ src/Components/RepoCard/RepoCard.js
@@ -6,18 +6,16 @@ const RepoCard = ({ repo }) => {
 	const {name, language, description, starCount, forkCount, dateCreated, commitsUrl} = repo;
 
 	return (
-		// <Link to={`/Commits/${repo.name}`}>
+		<Link to={`/Commits/${repo.name}`}>
 			<div className='repo-card'>
-				<Link to={`/Commits/${repo.name}`}>
 					<h2>{name}</h2> 
-				</Link>
 				<p>{language}</p>
 				<p>{description}</p>
 				<p>Star Count: {starCount}</p>
 				<p>Fork Count: {forkCount}</p>
 				<p>Date Created: {(dateCreated).toDateString()}</p>
 			</div>
-		// </Link>
+		</Link>
 	)
 }
 

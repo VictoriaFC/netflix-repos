be60e5e0f1f926d773dfbcf453d2d16b75f91c4e
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-13 15:03:18 -0700

    Updated RepoCard styling

--- src/Components/RepoCard/RepoCard.js
+++ src/Components/RepoCard/RepoCard.js
@@ -7,19 +7,26 @@ const RepoCard = ({ orgName, repo }) => {
 
 	return (
 			<div className='repo-card'>
-				<div className='left-side'>
-					<Link to={`/${orgName}/Commits/${name}`} style={{ textDecoration: 'none' }}>
-						<h2 className='repo-card-name'>{name}</h2> 
-					</Link>
-					<p className='repo-card-description'>{description}</p>
-					<div className='repo-card-info'>
-						<p className='language'>{language}</p>
-						<p>Created: {(dateCreated).toDateString()}</p>
+				<div className='top-section'>
+					<div className='left-side'>
+						<Link to={`/${orgName}/Commits/${name}`} style={{ textDecoration: 'none' }} target="_blank">
+							<h2 className='repo-card-name'>{name}</h2> 
+						</Link>
+						<p className='repo-card-description'>{description}</p>
+						<div className='repo-card-info'>
+							<p className='language'>{language}</p>
+							<p>Created: {(dateCreated).toDateString()}</p>
+						</div>
+					</div>
+					<div className='right-side'>
+						<p className='star-count'>⭐️ Star Count:<br></br> {starCount}</p>
+						<p className='fork-count'>🍴 Fork Count:<br></br> {forkCount}</p>
 					</div>
 				</div>
-				<div className='right-side'>
-					<p className='star-count'>⭐️ Star Count:<br></br> {starCount}</p>
-					<p className='fork-count'>🍴 Fork Count:<br></br> {forkCount}</p>
+				<div className='bottom-section'>
+					<Link to={`/${orgName}/Commits/${name}`} style={{ textDecoration: 'none' }} target="_blank">
+						<button className='repositories-button'>View Repository Commits</button>
+					</Link>
 				</div>
 			</div>
 

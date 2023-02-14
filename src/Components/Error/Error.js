9edd8490dee75c68e7a9126d92d33464b5a34035
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-13 21:04:49 -0700

    Added error image

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Error.css'
+import errorImage from '/Users/victoriafox-collis/turing/frontend/mod5/netflix-repos/src/assets/Hmmm....png'
 
 const Error = () => {
 	return (
 			<div className='error-container'>
+				<img src={errorImage} className='error-image'></img>
 				<h2 className='error-msg'>No Results Found</h2>
 				<p>We couldn't find what you searched for.</p>
 				<p>Try searching again.</p>

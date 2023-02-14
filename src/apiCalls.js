423eac24252994f1a332fd43d115acb7335f86a8
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-14 09:56:56 -0700

    Updated error handling for repo and commit length of 0

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -18,6 +18,8 @@ export const fetchRepos = async (org, setError) => {
 		
 		if(!response.ok) {
 			throw Error('No organization under that name exists')
+		} if(repos.length <= 0) {
+			throw Error('No repositories found')
 		} else {
 			return repos;
 		}
@@ -34,6 +36,8 @@ export const fetchCommits = async (org, repoName, setError) => {
 		console.log(response);
 
 		if(!response.ok) {
+			throw Error('no commits found')
+		} if(commits.length <= 0) {
 			throw Error('no commits found')
 		} else {
 			console.log('There were no errors')

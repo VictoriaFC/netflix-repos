ea930c8259eae49657a6061452711f3df7b94850
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-02-14 16:35:13 -0700

    Updated apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,15 +1,3 @@
-// export const fetchRepos = async (org) => {
-//   const response = await fetch(`https://api.github.com/orgs/${org}/repos`);
-// 	const repos = await response.json();
-// 	return repos;
-// }
-
-// export const fetchCommits = async (org, repoName) => {
-// 	const response = await fetch(`https://api.github.com/repos/${org}/${repoName}/commits`)
-// 	const commits = await response.json();
-// 	return commits;
-// }
-
 export const fetchRepos = async (org, setError) => {
 	try {
 		const response = await fetch(`https://api.github.com/orgs/${org}/repos`);
